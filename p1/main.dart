8799f7247f58cd10c48f8a18332dfe7de7c05d1e
FarrelEdric <franciscusfarrel14@gmail.com>
2024-09-09 14:42:34 +0700

    Pertemuan 2

---
+++ p1/main.dart
@@ -0,0 +1,6 @@
+void main() {
+  String nama = "Fulan"; // Definisikan variabel untuk nama
+  for (int umur = 18; umur >= 9; umur--) {
+    print("Nama saya adalah $nama, sekarang berumur $umur");
+  }
+}
