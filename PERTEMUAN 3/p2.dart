2b2c4fd56484a0d40f9cf926a2e99932cf4dc3cb
FarrelEdric <franciscusfarrel14@gmail.com>
2024-09-23 14:48:57 +0700

    Add files via upload

---
+++ PERTEMUAN 3/p2.dart
@@ -0,0 +1,12 @@
+void main() {
+  int counter = 0; // Inisialisasi variabel counter
+  
+  while (counter < 33) {
+    print(counter); // Cetak nilai counter
+    counter++;      // Tambah nilai counter
+  }
+  do {
+  print(counter);
+  counter++;
+} while (counter < 77);
+}
